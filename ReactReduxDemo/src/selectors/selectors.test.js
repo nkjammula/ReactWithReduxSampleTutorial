914c7db7c42c8a43a7ca74debdf655529d641f65
Naveen Jammula <noblenaveen@gmail.com>
2018-09-26 14:14:58 -0500

    Initial Commit for React Redux Practice Tutorial
    Initial Commit for React Redux Practice Tutorial

---
+++ ReactReduxDemo/src/selectors/selectors.test.js
@@ -0,0 +1,19 @@
+import expect from 'expect';
+import {authorsFormattedForDropdown} from './selectors';
+
+describe('Author Selectors',() => {
+    describe('authorsFormattedForDropDown',()=>{
+        it('should return author data formatted for use in dropdown',() =>{
+              const authors =[
+                  {id:'cory-house', firstName:'Cory',lastName:'House'},
+                  {id:'scott-allen', firstName:'Scott',lastName:'Allen'}
+              ];
+              const expected =[
+                {value:'cory-house', text:'Cory House'},
+                {value:'scott-allen', text:'Scott Allen'}
+              ];
+
+       expect(authorsFormattedForDropdown(authors)).toEqual(expected);
+    });
+});
+});
